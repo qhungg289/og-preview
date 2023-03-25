ddcfe4f5f93ad293f5ba750a0cce240c8f394a8d
Hung Duong Quang <qhungg289@protonmail.com>
2023-03-26 00:35:30 +0700

    add og api route

---
+++ src/pages/api/og.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+export default function handler(req, res) {
+	if (req.method == "POST") {
+		if (!req.body.url) {
+			res.status(400).json({ message: "URL is required" });
+		}
+
+		axios
+			.get(req.body.url)
+			.then((d) => {
+				const $ = cheerio.load(d.data);
+
+				const ogTitle = $('meta[property="og:title"]').attr("content");
+				const ogDescription = $('meta[property="og:description"]').attr(
+					"content",
+				);
+				const ogImage = $('meta[property="og:image"]').attr("content");
+
+				res.status(200).json({ ogTitle, ogDescription, ogImage });
+			})
+			.catch((_) => res.status(500).send());
+	}
+}
