9704b5bce748a6b25d5e5cb0b7ea1374bb6328b5
Hung Duong Quang <qhungg289@protonmail.com>
2023-03-26 19:57:23 +0700

    move footer to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,8 +5,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
 	return (
-		<main className={inter.className}>
-			<Component {...pageProps} />
-		</main>
+		<>
+			<main className={inter.className}>
+				<Component {...pageProps} />
+			</main>
+			<footer className="pb-6 text-center">
+				<p className="text-sm text-gray-600">
+					Made by{" "}
+					<a
+						href="https://github.com/qhungg289"
+						className="text-blue-600 transition-opacity hover:opacity-80"
+					>
+						@qhungg289
+					</a>
+				</p>
+			</footer>
+		</>
 	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -192,18 +192,6 @@ export default function Home() {
 					</div>
 				)}
 			</div>
-
-			<footer className="pb-6 text-center">
-				<p className="text-sm text-gray-600">
-					Made by{" "}
-					<a
-						href="https://github.com/qhungg289"
-						className="text-blue-600 transition-opacity hover:opacity-80"
-					>
-						@qhungg289
-					</a>
-				</p>
-			</footer>
 		</>
 	);
 }
