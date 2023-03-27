e8234cedd02bbf1b032c956b1164681f2f0e7818
Hung Duong Quang <qhungg289@protonmail.com>
2023-03-27 22:40:57 +0700

    update layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import GitHubCorner from "@/components/GitHubCorner";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
@@ -11,17 +12,7 @@ export default function App({ Component, pageProps }) {
 			<main className={inter.className}>
 				<Component {...pageProps} />
 			</main>
-			<footer className="pb-6 text-center">
-				<p className="text-sm text-gray-600">
-					Made by{" "}
-					<a
-						href="https://github.com/qhungg289"
-						className="text-blue-600 transition-opacity hover:opacity-80"
-					>
-						@qhungg289
-					</a>
-				</p>
-			</footer>
+			<Footer />
 		</>
 	);
 }
