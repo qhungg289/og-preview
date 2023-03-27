63cf8b77588a981405aa0a64515333ffe884a28f
Hung Duong Quang <qhungg289@protonmail.com>
2023-03-27 22:40:35 +0700

    add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+	return (
+		<footer className="pb-6 text-center">
+			<p className="text-sm text-gray-600">
+				Made by{" "}
+				<a
+					href="https://github.com/qhungg289"
+					className="text-blue-600 transition-opacity hover:opacity-80"
+				>
+					@qhungg289
+				</a>
+			</p>
+		</footer>
+	);
+}
