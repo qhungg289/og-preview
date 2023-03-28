009c83250f551819d26b60db2e7ce7a78cd565dc
Hung Duong Quang <qhungg289@protonmail.com>
2023-03-28 08:34:51 +0700

    change input from text to url and remove icon in it

--- src/pages/index.js
+++ src/pages/index.js
@@ -94,26 +94,12 @@ export default function Home() {
 					className="relative mx-auto flex max-w-lg"
 					onSubmit={handleSubmit}
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth={1.5}
-						stroke="currentColor"
-						className="absolute top-2 left-2 h-6 w-6 text-gray-300"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
-						/>
-					</svg>
 					<input
-						className="w-full rounded border border-gray-300 pr-28 pl-10 placeholder:text-gray-400 focus:border-gray-500 focus:ring-0"
-						type="text"
+						className="w-full rounded border border-gray-300 pr-28 placeholder:text-gray-400 focus:border-gray-500 focus:ring-0"
+						type="url"
 						name="url"
 						id="url"
-						placeholder="Your URL..."
+						placeholder="https://example.com"
 						value={url}
 						onChange={handleUrlInputChange}
 						ref={urlInputRef}
